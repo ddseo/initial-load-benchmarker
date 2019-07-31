afda28c5f38c20355252a492d76d62a8e3e7f8c0
Derek Seo <derek@thehive.ai>
2019-07-30 18:16:19 -0700

    project setup

---
+++ .eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    "comma-dangle": ["error", {
+      "arrays": "never",
+      "imports": "never",
+      "exports": "never",
+      "functions": "never",
+      "objects":"ignore"
+    }],
+    "prefer-destructuring": 0,
+    "import/no-extraneous-dependencies": 0,
+    "key-spacing": [
+      "error",
+      {
+        align: {
+          "beforeColon": true,
+          "afterColon": true,
+          "on": "colon"
+        }
+      }
+    ],
+    "no-multi-spaces": [
+      "error",
+      {
+        exceptions: {
+          "VariableDeclarator": true,
+          "ImportDeclaration": true
+        }
+      }
+    ],
+    "eqeqeq": [
+      "error",
+      "smart"
+    ],
+    "no-underscore-dangle": 0,
+    camelcase: 0,
+    "comma-dangle": ["error", "always-multiline"],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "no-console": 0,
+    "no-alert": 2,
+    "no-trailing-spaces": [
+      "error"
+    ],
+    "no-use-before-define": 0,
+    "no-unused-vars": [
+      "error",
+      {
+        "args": "none"
+      }
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error"
+    ],
+  }
+}
